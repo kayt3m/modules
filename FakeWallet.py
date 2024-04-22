e8fd4f1a34da56b8990f7fce8437ef2fe01050bc
nerv.love <123994724+kayt3m@users.noreply.github.com>
2024-04-22 17:20:11 +0300

    Update FakeWallet.py

--- FakeWallet.py
+++ FakeWallet.py
@@ -1,5 +1,5 @@
 #Name: FakeWallet
-#Description: Fun joke - fake crypto wallet.
+#Description: Fun joke - fake crypto wallet. You can change cryptocurrency values ​​using .cfg FakeWallet.
 #Author: @nervousmods
 #Commands:
 #.fwallet | .fwinfo
@@ -21,10 +21,9 @@ from telethon.tl.custom import Message
 __version__ = (1, 0, 0)
 logger = logging.getLogger(__name__)
 
-
 @loader.tds
 class FakeWallet(loader.Module):
-    """Fun joke - fake crypto wallet."""
+    """Fun joke - fake crypto wallet. You can change cryptocurrency values ​​using .cfg FakeWallet."""
     def __init__(self):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
@@ -105,10 +104,38 @@ class FakeWallet(loader.Module):
         "usdt": "Enter a value for USDT coin",
         "gram": "Enter a value for Gramcoin",
         "lite": "Enter a value for Litecoin",
+        "info": "<b><emoji document_id=5305467350064047192>🫥</emoji><i>Attention!</b>\n\n"
+                "<i><emoji document_id=5915991028430542030>☝️</emoji>This module is strictly prohibited from being used for the purposes of <b>scam, fraud and advertising</b>.\n\n"
+                "<emoji document_id=5787190061644647815>🗣</emoji>The module is provided solely for entertainment purposes, and any violation of the <b>Rules for using the module</b>, if detected, will be subject <b>to appropriate punishment</i>"
     }
 
-    @loader.command("fwallet")
-    async def fwallet(self, message: Message):
+    strings_ru = {
+        "wallet": "<emoji document_id=5438626338560810621>👛</emoji> <b>Кошелёк</b>\n\n"
+                  "<emoji document_id=5215276644620586569>☺️</emoji> <a href='https://ton.org'>Toncoin</a>: {} TON\n\n"
+                  "<emoji document_id=5215699136258524363>☺️</emoji> <a href='https://tether.to'>Tether</a>: {} USDT\n\n"
+                  "<emoji document_id=5215590800003451651>☺️</emoji> <a href='https://bitcoin.org'>Bitcoin</a>: {} BTC\n\n"
+                  "<emoji document_id=5217867240044512715>☺️</emoji> <a href='https://etherium.org'>Etherium</a>: {} ETH\n\n"
+                  "<emoji document_id=5215595550237279768>☺️</emoji> <a href='https://binance.org'>Binance coin</a>: {} BNB\n\n"
+                  "<emoji document_id=5215437796088499410>☺️</emoji> <a href='https://tron.network'>TRON</a>: {} TRX\n\n"
+                  "<emoji document_id=5215440441788351459>☺️</emoji> <a href='https://www.centre.io/usdc'>USD Coin</a>: {} USDC\n\n"
+                  "<emoji document_id=5215267041073711005>☺️</emoji> <a href='https://gramcoin.org'>Gram</a>: {} GRAM\n\n"
+                  "<emoji document_id=5217877586620729050>☺️</emoji> <a href='https://litecoin.org'>Litecoin</a>: {} LTC",
+        "ton": "Введите количество валюты для Toncoin",
+        "teth": "Введите количество валюты для Tethcoin",
+        "btc": "Введите количество валюты для Bitcoin",
+        "ether": "Введите количество валюты для Etherium",
+        "binc": "Введите количество валюты для Binance coin",
+        "tron": "Введите количество валюты для Tron",
+        "usdt": "Введите количество валюты для USDT coin",
+        "gram": "Введите количество валюты для Gramcoin",
+        "lite": "Введите количество валюты для Litecoin",
+        "info": "<b><emoji document_id=5305467350064047192>🫥</emoji><i> Внимание!</b>\n\n"
+                "<i><emoji document_id=5915991028430542030>☝️</emoji> Использование этого модуля в целях <b>скама, обмана и рекламы</b> строго запрещено.\n\n"
+                "<emoji document_id=5787190061644647815>🗣</emoji> Модуль предоставлен исключительно в развлекательных целях, и любое нарушение <b>Правил использования модуля</b>, если его обнаружат, будет подлежать соответствующему наказанию.</i>"
+    }
+
+    @loader.command()
+    async def fwalletcmd(self, message: Message):
         """-> to get a fake wallet"""
         ton = self.config["Toncoin"]
         teth = self.config["Tether"]
@@ -125,4 +152,4 @@ class FakeWallet(loader.Module):
     @loader.command()
     async def fwinfocmd(self, message):
         """-> info about FakeModule"""
-        await utils.answer(message, "<b><emoji document_id=5305467350064047192>🫥</emoji><i>Attention!</b>\n\n <emoji document_id=5915991028430542030>☝️</emoji>This module is strictly prohibited from being used for the purposes of <b>scam, fraud and advertising</b>.\n\n<emoji document_id=5787190061644647815>🗣</emoji>The module is provided solely for entertainment purposes, and any violation of the <b>Rules for using the module</b>, if detected, will be subject <b>to appropriate punishment</b></i>.")
+        await utils.answer(message, self.strings("info"))
